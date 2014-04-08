f6f230b165504ca896d840c3925d724fa9508c1d
campbelg <campbelg@oclc.org>
2014-04-08 11:21:56 -0400

    Remove bash service files from project

--- examples/runtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#
-# Run all the tests for the examples
-#
-python -m authentication_token.tests.access_token_formatter_test
-python -m authentication_token.tests.bibliographic_record_test
-python -m authentication_token.tests.server_test
-python -m authentication_token.tests.session_handler_test
