e9285ff0f085194451973667480dce5ae4a3c9a5
campbelg <campbelg@oclc.org>
2014-09-09 14:19:37 -0400

    Added a DAPI request to the Client Credentials Grant example.

--- examples/client_credentials_grant/client_credentials_grant.py
+++ examples/client_credentials_grant/client_credentials_grant.py
@@ -19,6 +19,7 @@
 
 # Example of retrieving a token with Client Credentials Grant
 
+import urllib2
 from authliboclc import wskey
 
 #
@@ -27,21 +28,31 @@ from authliboclc import wskey
 
 key = '{clientID}'
 secret = '{secret}'
-authenticating_institution_id = '{institutionID}'
-context_institution_id = '{institutionID}'
+authenticating_institution_id = '{institutionId}'
+context_institution_id = '{institutionId}'
 
+# Configure the wskey library object
 my_wskey = wskey.Wskey(
     key=key,
     secret=secret,
     options={'services': ['WorldCatDiscoveryAPI']})
 
-access_token = my_wskey.get_access_token_with_client_credentials(authenticating_institution_id, context_institution_id)
+# Get an access token
+access_token = my_wskey.get_access_token_with_client_credentials(
+    authenticating_institution_id=authenticating_institution_id,
+    context_institution_id=context_institution_id
+)
 
+# Describe the token received, or the error produced
 print("")
 if (access_token.access_token_string == None):
-    print("error_code:    " + `access_token.error_code`);
-    print("error_message: " + access_token.error_message);
-    print("error_url:     " + access_token.error_url);
+    if (key == '{clientID}'):
+        print(
+        "**** You must configure the key, secret, authenticating_institution_id and context_institution_id ****")
+        print("")
+    print("error_code:    " + `access_token.error_code`)
+    print("error_message: " + access_token.error_message)
+    print("error_url:     " + access_token.error_url)
 else:
     print("access token:  " + access_token.access_token_string)
     print("expires_in:    " + `access_token.expires_in`)
@@ -50,3 +61,24 @@ else:
     if (access_token.refresh_token != None):
         print("refresh_token: " + access_token.refresh_token)
 print("")
+
+# Make a DAPI Request - get a JSON Bib Record back for OCLC #52356689
+if (access_token.access_token_string != None):
+    request_url = 'https://beta.worldcat.org/discovery/bib/data/52356689'
+    authorization = 'Bearer ' + access_token.access_token_string
+    print("Making a request to " + request_url)
+    print(authorization)
+
+    my_request = urllib2.Request(
+        url=request_url,
+        data=None,
+        headers={'Authorization': authorization, 'Accept': 'application/json'}
+    )
+
+    try:
+        result = urllib2.urlopen(my_request).read()
+
+    except urllib2.HTTPError, e:
+        result = str(e)
+
+    print(result)
